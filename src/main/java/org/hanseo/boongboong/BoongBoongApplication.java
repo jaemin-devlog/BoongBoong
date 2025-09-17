449c094761113a582305e26b27349f15a3b71286
재민 <jjm0203311@naver.com>
2025-09-17 18:26:01 +0900

    :tada: 초기 프로젝트 설정

---
+++ src/main/java/org/hanseo/boongboong/BoongBoongApplication.java
@@ -0,0 +1,13 @@
+package org.hanseo.boongboong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoongBoongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoongBoongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hanseo/boongboong/BoongBoongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hanseo.boongboong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoongBoongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
