af5e8625219b6d40efc23093e418ed86908c1579
재민 <jjm0203311@naver.com>
2025-09-19 23:18:14 +0900

    스프링 부트 애플리케이션 실행 진입점

--- src/main/java/org/hanseo/boongboong/BoongBoongApplication.java
+++ src/main/java/org/hanseo/boongboong/BoongBoongApplication.java
@@ -2,8 +2,11 @@ package org.hanseo.boongboong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(exclude = {ThymeleafAutoConfiguration.class})
 public class BoongBoongApplication {
 
     public static void main(String[] args) {
