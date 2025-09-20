924c111be01a2ca3e2801c730fcf2e14bcd435a4
재민 <jjm0203311@naver.com>
2025-09-20 20:37:10 +0900

    관리자 기능 삭제

--- src/main/java/org/hanseo/boongboong/domain/user/entity/Role.java
+++ src/main/java/org/hanseo/boongboong/domain/user/entity/Role.java
@@ -7,8 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Role {
     USER("USER", "일반 사용자"),
-    DRIVER("DRIVER", "운전자"),
-    ADMIN("ADMIN", "관리자");
+    DRIVER("DRIVER", "운전자");
 
     private final String authority;   // 권한 코드 (스프링 시큐리티에서 ROLE_ 접두사와 함께 사용됨)
     private final String description; // 권한 설명 (한글로 역할 의미를 표시)
