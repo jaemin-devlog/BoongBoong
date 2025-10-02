d0e3a448feb315aef2ed015e794b719263bc8fbf
재민 <jjm0203311@naver.com>
2025-10-02 19:08:41 +0900

    알림 타입 정의

---
+++ src/main/java/org/hanseo/boongboong/notify/NotificationType.java
@@ -0,0 +1,7 @@
+package org.hanseo.boongboong.notify;
+
+// src/main/java/.../notify/NotificationType.java
+public enum NotificationType {
+    NEW_REQUEST, REQUEST_APPROVED, REQUEST_REJECTED, REQUEST_CANCELLED,
+    MATCH_LOCKED, ATTENDED, NO_SHOW, MATCH_COMPLETED, COUNTERS_UPDATED, SYSTEM
+}
