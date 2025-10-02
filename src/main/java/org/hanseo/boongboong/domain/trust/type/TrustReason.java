182f91ae36ab09cf0ccf57714fe145d4f4154143
재민 <jjm0203311@naver.com>
2025-10-02 19:19:51 +0900

    신뢰도 변화 사유 Enum

---
+++ src/main/java/org/hanseo/boongboong/domain/trust/type/TrustReason.java
@@ -0,0 +1,7 @@
+// src/main/java/org/hanseo/boongboong/domain/trust/type/TrustReason.java
+package org.hanseo.boongboong.domain.trust.type;
+
+public enum TrustReason {
+    SAME_DAY_CANCEL, // 당일취소 -5
+    NO_SHOW          // 노쇼 -10
+}
