0c27d0a825b12ca2239efa7ad735aa0c6f6420e1
재민 <jjm0203311@naver.com>
2025-12-04 20:15:39 +0900

    운영환경 세팅

--- src/main/java/org/hanseo/boongboong/domain/carpool/dto/response/AuthorDto.java
+++ src/main/java/org/hanseo/boongboong/domain/carpool/dto/response/AuthorDto.java
@@ -2,13 +2,12 @@ package org.hanseo.boongboong.domain.carpool.dto.response;
 
 import lombok.*;
 
-// ??• : ?‘ì„±???•ë³´ë¥?API ?‘ë‹µ?¼ë¡œ ?„ë‹¬?˜ëŠ” ê°€ë²¼ìš´ DTO (id, ?‰ë„¤?? ?„ë¡œ???´ë?ì§€)
-// ?¬ìš©ì²? Carpool ?‘ë‹µ ë°”ë””?ì„œ ?‘ì„±??ë¸”ë¡???½ì…
+
 @Getter @Builder @NoArgsConstructor @AllArgsConstructor
 public class AuthorDto {
-    private Long id;          // ?‘ì„±??ID
-    private String nick;      // ?‘ì„±???‰ë„¤???œë¹„???¸ì¶œëª?
+    private Long id;
+    private String nick;
     private String profileImg;
-    private int trustScore;    // ½Å·Ú Á¡¼ö // ?„ë¡œ???´ë?ì§€(Data URL ?ëŠ” URL)
+    private int trustScore;
 }
 
